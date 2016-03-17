f71bf51a5dab5630c285d338a2f0feb8b242a5ea
chuawh <chuawh.74@gmail.com>
2016-03-17 22:38:16 +0800

    Create textToCreateSparkRoom.js

---
+++ textToCreateSparkRoom.js
@@ -0,0 +1,46 @@
+importPackage(java.net);
+importPackage(java.io);
+importPackage(java.util);
+
+function post(urlString, body) {
+    var url = new URL(urlString);
+    log("Opening connection.");
+    var connection = url.openConnection();
+    connection.setRequestMethod("POST");
+    connection.setDoOutput(true);
+    connection.setRequestProperty('Content-Type', 'application/json');
+    connection.setRequestProperty('Authorization', 'Bearer MjliOTQyMDgtODMzZS00NWZjLWEyOWQtODljYTM2ZGMzN2I4OGE0ZmQzYzItNTk4');
+
+    log("Sending output.");
+    var output = new DataOutputStream(connection.getOutputStream());
+    output.writeBytes(body);
+    output.flush();
+    output.close();
+    
+    var responseCode = connection.getResponseCode();
+    log("Response is: " + responseCode);
+
+    var scanner = new Scanner(connection.getInputStream(), "UTF-8").useDelimiter("\\A");  
+    var result = scanner.next();
+    log("Result is: " + result);
+    scanner.close();
+    return [responseCode, result];  
+}
+
+var roomName=currentCall.initialText;
+var jsonRoomName={'title':roomName};
+log("********** The Room Name is ----> " + roomName + "*******************");
+var httpResponse= post("https://api.ciscospark.com/v1/rooms",JSON.stringify(jsonRoomName));
+log("ResponseCode is:" + httpResponse[0]);
+log("The Spark Response is:" + httpResponse[1]);
+
+//var callerNumber=string(currentCall.callerID);
+//var finalNumber=Number(callerNumber.slice(1,8));
+//log("The finalNumber is" + finalNumber);
+
+if (httpResponse[0]==200) {
+message("The Spark Room named " + roomName + " has successfully created", {
+	    to:"+6597809414",
+	    network:"SMS"
+	});
+}

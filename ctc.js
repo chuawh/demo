fc6745992a488ff491ccb9fb422f824a5589212a
chuawh <chuawh.74@gmail.com>
2016-03-18 13:55:22 +0800

    Update ctc.js

--- ctc.js
+++ ctc.js
@@ -31,7 +31,7 @@ var result=ask("If you would like to speak to customer, Then Press 1. Else, pres
 
 if (result.value==1){
       say( "Please wait while we transfer your call");
-      transfer("+6597809414");
+      transfer('+6597809414');
       }  
       else {
             say( "Goodbye" );

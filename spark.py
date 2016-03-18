7f9b05c14af9164d5a5239890903d27363479d22
chuawh <chuawh.74@gmail.com>
2016-03-18 09:49:25 +0800

    Create spark.py

---
+++ spark.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+token="Bearer MjliOTQyMDgtODMzZS00NWZjLWEyOWQtODljYTM2ZGMzN2I4OGE0ZmQzYzItNTk4"
+roomName="ABC"
+
+
+def _url(path):
+    return 'https://api.ciscospark.com/v1' + path
+    
+#POST Requests
+def post_createroom(at,title):
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'title':title}
+    resp = requests.post(url=_url('/rooms'),json=payload, headers=headers)
+    dict = json.loads(resp.text)
+    dict['statuscode']=str(resp.status_code)
+    return dict
+    
+post_createroom(token,roomName)

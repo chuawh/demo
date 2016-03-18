d67a0da85bd2d735ddab05be0867788af8c1e4ab
chuawh <chuawh.74@gmail.com>
2016-03-18 12:43:19 +0800

    Update ctc.js

--- ctc.js
+++ ctc.js
@@ -9,9 +9,9 @@ call('sip:whongchu@cisco.com', {
 	       wait(500);
 	       say("The use of vehicle is" + UseofVehicle);
 	       wait(500);
-	       say("The caller number is:" + moblie);
+	       say("The caller number is:" + mobile);
 	       wait(500);
-	       talkToClient(mobie);
+	       talkToClient(mobile);
 	       log("Obnoxious call complete");
 	   },
 	   onTimeout: function() {

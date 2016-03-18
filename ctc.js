807f6a7b51e54708a5a5ffc7ad3f1edf85827115
chuawh <chuawh.74@gmail.com>
2016-03-18 12:40:30 +0800

    Update ctc.js

--- ctc.js
+++ ctc.js
@@ -11,6 +11,7 @@ call('sip:whongchu@cisco.com', {
 	       wait(500);
 	       say("The caller number is:" + moblie);
 	       wait(500);
+	       talkToClient(mobie);
 	       log("Obnoxious call complete");
 	   },
 	   onTimeout: function() {
@@ -21,7 +22,7 @@ call('sip:whongchu@cisco.com', {
 	   }
 	});
 
-function talkToClient(){
+function talkToClient(nbrToDial){
 var result=ask("If you would like to speak to customer, Then Press 1 ", {
     choices:"[1]",
     terminator:"#",
@@ -29,8 +30,14 @@ var result=ask("If you would like to speak to customer, Then Press 1 ", {
     mode:"dtmf",
     interdigitTimeout: 5,});
 
-       if (result.value=="1")
-            say( "Please wait while we transfer your call");
+    if (result.value=="1"){
+         say( "Please wait while we transfer your call");
+         transfer(nbrToDial, {
+            playvalue: "http://www.phono.com/audio/holdmusic.mp3",
+            terminator: "*",
+            onTimeout: function(event) {
+         say("Sorry, but nobody answered");}
+         }
        else
             say( "Goodbye" );
             hangup();

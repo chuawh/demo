40c7838773abd82d2c508c71702b0524b1d1479c
chuawh <chuawh.74@gmail.com>
2016-03-24 20:58:29 +0800

    Create ctc2.js

---
+++ ctc2.js
@@ -0,0 +1,18 @@
+
+var customerNbr=mobile;
+var agentNbr=agentNumber;
+ 
+call('+' + agentNbr , {
+	   timeout:60,
+	      onAnswer: function() {
+	       say("Pleawe wait while we tranfer your call to the customer");
+	       log("Obnoxious call complete");
+	   },
+	   onTimeout: function() {
+	       log("Call timed out");
+	   },
+	   onCallFailure: function() {
+	       log("Call could not be completed as dialed");
+	   }
+	});
+transfer('+' + customerNbr);

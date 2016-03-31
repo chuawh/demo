8dd22812eec3321b283c8c5ad76670bdfd9fc6ed
chuawh <chuawh.74@gmail.com>
2016-03-31 12:03:03 +0800

    Update sensorTagDemo.js

--- sensorTagDemo.js
+++ sensorTagDemo.js
@@ -1,6 +1,8 @@
+
 var nbrToDial=mobile;
  
 call('+' + nbrToDial , {
+	   network:"SMS",
 	   timeout:60,
 	      onAnswer: function() {
 	       say("This is a test call from C C 2 6 5 0 wireless MCU sensor device. An emergency alarm has been activated. Please join the emergency team Spark room now.Thank you");

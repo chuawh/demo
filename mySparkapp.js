c7c6766ad016579737f4a8ad9bad42fea762ed40
chuawh <chuawh.74@gmail.com>
2016-04-04 11:22:07 +0800

    Update mySparkapp.js

--- mySparkapp.js
+++ mySparkapp.js
@@ -2,7 +2,7 @@ var customerNbr=mobile;
 var sparkRoomSipUri=sparkNumber;
  
 call(customerNbr, {
-	   timeout:60,
+	   timeout:120,
 	      onAnswer: function() {
 	       say("Please wait while we connect your call to Cisco Spark Room");
 	       log("Obnoxious call complete");

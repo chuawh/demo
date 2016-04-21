9ec7913580787bc06f4a9468e1c5001d94305c66
chuawh <chuawh.74@gmail.com>
2016-04-21 13:49:45 +0800

    Update ctc3.js

--- ctc3.js
+++ ctc3.js
@@ -23,7 +23,7 @@ var result=ask("What would you like to do? Please choose from the following opti
 say("You chose" + result.value);
 
 
-if (result.value==1){
+if (result.value=="1"){
     message("Please join the webex meeting at http://acecloud.webex.com, Host PIN: 1234", {
     to:"+6597809414",
     network:"SMS"
